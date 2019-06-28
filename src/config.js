18892a795d4b0d0a4c42e8c7432662f20dc083b7
Michael Verdi <michael.v.verdi@gmail.com>
2019-06-28 11:16:40 -0400

    adding favicons, adding deploy scripts, adding now.json file

--- src/config.js
+++ src/config.js
@@ -1,4 +1,14 @@
+let apiPath;
+let tokenKey;
+if (process.env.NODE_ENV === 'production') {
+  apiPath = 'https://bonjour-api.herokuapp.com/api'
+  tokenKey = 'bonjour-app-prod-auth-token'
+} else {
+  apiPath = 'http://localhost:8000/api'
+  tokenKey = 'bonjour-app-dev-auth-token'
+}
+
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  TOKEN_KEY: 'blogful-client-auth-token',
+  API_ENDPOINT: apiPath,
+  TOKEN_KEY: tokenKey,
 }
